0f8f61e7b7e1ff432b7108b6ca6781a74912d4c2
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-05-31 12:12:00 +0200

    Add script.js for functionality

---
+++ script.js
@@ -0,0 +1,53 @@
+const nameInput = document.getElementById('student-name');
+const markInput = document.getElementById('student-mark');
+const buttonElement = document.getElementById('submit-button');
+const resultElement = document.getElementById('results');
+const studentsList = document.getElementById('student-list');
+
+
+buttonElement.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    //Read the input
+    const name = nameInput.value.trim();
+    const mark = markInput.value.trim();
+
+    
+
+    if (name===""||
+        mark=== "" ||
+        Number(mark) <0 ||
+        Number(mark) >100 
+
+    ) {
+        resultElement.textContent = "Put name and mark";
+        return;
+    }
+
+    
+    
+    let result;
+    const markValue = Number(mark);
+    
+
+    if(markValue >= 80 && markValue <= 100){
+        result = "Passed with distinction"
+    } else if(markValue >= 65 && markValue <= 79){
+        result = "Passed with merit"
+    } else if(markValue >= 50 && markValue <= 64){
+        result = "Passed"
+    } else{
+        result = "Failed"
+    }
+
+    resultElement.innerHTML = result;
+
+
+    const listItem = document.createElement("li");
+    listItem.textContent = name + " (" + markValue + "%) : " + result;
+    studentsList.appendChild(listItem);
+    
+
+});
+
